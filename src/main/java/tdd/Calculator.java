b0dae63a398a89336ff153d3fc9ed462b7fef40a
Raymart De Guzman <tramyardg@gmail.com>
2019-01-10 12:35:27 -0500

    added slf4j for logging, testAverage: make it pass

--- src/main/java/tdd/Calculator.java
+++ src/main/java/tdd/Calculator.java
@@ -1,13 +1,31 @@
 package tdd;
 
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
 
-    public void addElement(double d) {
+    private ArrayList<Double> values;
+    private final Logger log = LoggerFactory.getLogger(Calculator.class);
 
+    public Calculator() {
+	this.values = new ArrayList<>();
+    }
+
+    public void addElement(double d) {
+	log.info("adding element {}", d);
+	this.values.add(d);
     }
 
     public double average() {
-	return Double.NaN;
+	log.info("calculating average...");
+	double totalVal = 0.0;
+	for (int i = 0; i < values.size(); i++) {
+	    totalVal += values.get(i);
+	}
+	log.info("expected value {}", (totalVal / values.size()));
+	return (totalVal / values.size());
     }
 
 }
